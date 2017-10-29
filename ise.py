71dbedd262666ed10e8c346310fc5e439e00fc2a
Justin Pugliese <jmpuglie@us.ibm.com>
2017-10-29 12:08:21 -0400

    initial structure

---
+++ ise.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def requery():
+    """Select unused, high-confidence, tuple then query and process."""
+    pass
+
+def query():
+    """Send request to Google Custom Search endpoint."""
+    # process
+    pass
+
+def process():
+    """Process each result from Google search."""
+    # check if already processed
+    # process new url
+        # fetch HTML
+        # extract plain text
+        # annotate text
+        # identify tuples
+
+    # check if done
+    # else, requery
+    pass
+
+def fetchHTML():
+    """Fetch url HTML."""
+    pass
+
+def extractText():
+    """Strip markdown."""
+    pass
+
+def annotate():
+    """Annotate text with the Stanford CoreNLP."""
+    pass
+
+def identify():
+    """Identify new tuples."""
+    # identify tuples
+    # check if they've already been identified
+    pass
+
+def progressCheck():
+    """Done if k matches."""
+    pass
+
+
+def main():
+    """Main entry point for the script."""
+
+    # do code
+    # print_parameters()
+    # query
+
+    print("Finished")
+
+if __name__ == '__main__':
+    sys.exit(main())
